b0dd60072a949c09b786c080163a229c19a4c372
ArtrenH <79058115+ArtrenH@users.noreply.github.com>
2023-05-21 14:03:19 +0200

    first bokeh tests

--- plots.py
+++ plots.py
@@ -1,6 +1,7 @@
 import os
 import json
 from tqdm import tqdm
+from bokeh.plotting import figure, show
 import matplotlib.pyplot as plt
 
 plt.style.use("Solarize_Light2")
@@ -9,9 +10,7 @@ def gather_period_lengths() -> list[int]:
     all_files = os.listdir("data/cleaned_content")
     return [0] + [len([elem for elem in all_files if elem.split("_")[2] == str(i)]) for i in range(1, 7+1)]
 
-def plot_topic(topic: str) -> None:
-    plt.clf()
-    os.makedirs("data/plots", exist_ok=True)
+def gather_topic_data(topic: str):
     period_lengths = gather_period_lengths()
     with open(f"data/topic_keywords_stemmed/{topic}_Daten.json", "r") as f:
         data: dict = json.load(f)
@@ -22,13 +21,25 @@ def plot_topic(topic: str) -> None:
         scores.append(
             sum([sum(elem.values()) for elem in period_data.values()])/period_lengths[int(period)]
         )
+    return periods, scores
+    
+
+def plot_topic(topic: str) -> None:
+    plt.clf()
+    os.makedirs("data/plots", exist_ok=True)
     plt.title(f"Wort-Cluster-Score pro Sitzung für Thema {topic}")
     plt.ylabel(f"tf-idf-score sum per sitzung")
     plt.xlabel("Wahlperiode")
-    plt.bar(periods, scores)
+    plt.bar(*gather_topic_data(topic))
     plt.xlim([0, 8])
     plt.tight_layout()
     plt.savefig(f"data/plots/{topic}.png", dpi=500)
+
+def bokeh_plot(topic: str) -> None:
+    p = figure(title=f"Analye der Scores für Wortfeld {topic}", x_axis_label="Wahlperiode", y_axis_label="tf-idf-score Summe pro Sitzung")
+    periods, scores = gather_topic_data(topic)
+    p.vbar(x=periods, top=scores, legend_label="tf-idf-scores", width=0.5, bottom=0, color="violet")
+    show(p)
 
 def plot_all_topics() -> None:
     with open("raw_data/topics.json", "r") as f:
